e5c77534d6d60ca8222926db9e9bb0751cf5cef1
Jameson Nash <vtjnash@gmail.com>
2017-11-21 23:21:50 -0500

    libuv: upgrade to v2 master fork
    also make RawFD and WindowsRawSocket valid as ccall types
    and fix which fd is being passed to `fdio` in `init_stdio`

--- base/deprecated.jl
+++ base/deprecated.jl
@@ -1412,6 +1412,10 @@ end
 
 @deprecate print_with_color(color, args...; kwargs...) printstyled(args...; kwargs..., color=color)
 
+# PR 25062
+@deprecate(link_pipe(pipe; julia_only_read = true, julia_only_write = true),
+           link_pipe!(pipe, reader_supports_async = julia_only_read, writer_supports_async = julia_only_write))
+
 # END 0.7 deprecations
 
 # BEGIN 1.0 deprecations
